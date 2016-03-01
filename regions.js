e6f29d5b165346a32db079c09aa5788e6a6cfe74
John Frimmel <jrfrimme@unca.edu>
2016-03-01 16:23:08 -0500

    updates paths. adds wrapper for hawaii. adds links

--- regions.js
+++ regions.js
@@ -1,4 +1,14 @@
-$("path").hover(
+$(".map-region").hover(
+    function (e) {
+        var region = $(this).data("region");
+        $("#descriptions").removeClass().addClass(region);
+    },
+    function (e) {
+        $("#descriptions").removeClass();
+    }
+);
+
+$(".map-region").focus(
     function (e) {
         var region = $(this).data("region");
         $("#descriptions").removeClass().addClass(region);
