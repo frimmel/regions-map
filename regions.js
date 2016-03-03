20109adf2f1911bf493ab9d2607649af918fdc07
John Frimmel <jrfrimme@unca.edu>
2016-03-03 13:38:49 -0500

    text fixes for ie

--- regions.js
+++ regions.js
@@ -1,10 +1,25 @@
-$(".map-region").hover(
+/*
+$("a").hover(
     function (e) {
         var region = $(this).data("region");
         $("#descriptions").removeClass().addClass(region);
     },
     function (e) {
         $("#descriptions").removeClass();
+    }
+);
+*/
+
+$(".map-region").hover(
+    function (e) {
+        var region = $(this).data("region");
+        console.log(region)
+        $("#descriptions").removeClass().addClass(region);
+        console.log("hover in")
+    },
+    function (e) {
+        $("#descriptions").removeClass();
+        console.log("hover out")
     }
 );
 
